1f2c2e9c79b3e1d83b588a5bd57c111484c2c434
Quentin Duminy <60063118+Onilaf@users.noreply.github.com>
2026-07-30 15:08:10 +0200

    Fix Last Change

--- Assets/Scripts/PatientData/Steps/StepTelephone.cs
+++ Assets/Scripts/PatientData/Steps/StepTelephone.cs
@@ -6,23 +6,37 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace PatientData.Steps{
-    public class StepTelephone : MonoBehaviour {
+    public class StepTelephone : MonoBehaviour{
         [SerializeField] private Image patientSpritePhone;
         [SerializeField] private GameObject bulleDiscussion;
         [SerializeField] private Transform content;
-        
+
+        private Image _lastBubble;
+
         public void SetSprites(Sprite patient){
             patientSpritePhone.sprite = patient;
         }
-        
+
         public IEnumerator StartConv(List<Discussion> discussion){
             foreach (Discussion d in discussion) {
                 GameObject go = Instantiate(bulleDiscussion, content);
+
                 go.GetComponentInChildren<TextMeshProUGUI>().text = d.text;
+
                 if (d.qui == "Patient") {
                     go.transform.localScale = new Vector3(-1, 1, 1);
                     go.transform.GetChild(0).localScale = new Vector3(-1, 1, 1);
                 }
+
+                // Retire la surbrillance de l'ancienne bulle
+                if (_lastBubble != null) {
+                    _lastBubble.color = Color.white;
+                }
+
+                // Met en surbrillance la nouvelle
+                _lastBubble = go.GetComponent<Image>();
+                _lastBubble.color = Color.yellow;
+
                 yield return new WaitForSeconds(3f);
             }
         }
